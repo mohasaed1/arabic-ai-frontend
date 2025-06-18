f4129cc07ffbdae5184a275808ad393810f07600
mohasaed1 <125800065+mohasaed1@users.noreply.github.com>
2025-06-18 21:42:51 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,29 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+  },
   server: {
+    host: '0.0.0.0',
     port: 3000,
   },
   build: {
+    target: 'esnext',
     outDir: 'dist',
-    sourcemap: true,
+    sourcemap: false,
   },
-  base: './',
+  css: {
+    preprocessorOptions: {
+      css: {
+        charset: false,
+      },
+    },
+  },
 });
