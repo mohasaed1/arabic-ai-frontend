ac0e3da8355f4fd54a061ca29e15530e57a3c862
mohasaed1 <125800065+mohasaed1@users.noreply.github.com>
2025-06-21 15:20:07 +0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,5 +3,25 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './theme.css';
 
+// Chart.js elements registration
+import {
+  Chart as ChartJS,
+  ArcElement,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+
+ChartJS.register(
+  ArcElement,
+  BarElement,
+  CategoryScale,
+  LinearScale,
+  Tooltip,
+  Legend
+);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
